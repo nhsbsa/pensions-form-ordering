fcf830063ebcbe485bf48082060accb887b53edd
“Thomas <thomas.leeds@nhsbsa.nhs.uk>
2024-06-06 14:13:21 +0100

    First commit

---
+++ app/routes.js
@@ -0,0 +1,17 @@
+// External dependencies
+const express = require('express');
+const router = express.Router();
+
+// clear session data - link in footer
+router.post('/clear-session-data/', (req, res) => {
+    req.session.data = {}
+    res.render('index')
+})
+
+// =======================================
+// Version Routes Files Below
+// =======================================
+
+router.use('/v1', require('./views/v1/_routes'));
+
+module.exports = router;
---
+++ app/views/v1/_routes.js
@@ -0,0 +1,24 @@
+// External dependencies
+const e = require('express');
+const express = require('express');
+const router = express.Router();
+
+// Add your routes here - above the module.exports line
+
+// ========================================================================
+// MAIN ONLINE CONTACT FORM JOURNEY
+// ========================================================================
+
+
+// Start page
+router.post(/start/, (req, res) => {
+    res.redirect('date-of-birth')
+});
+
+// What is your date of birth?
+router.post(/date-of-birth/, (req, res) => {
+    res.redirect('previous-contracts')
+});
+
+
+module.exports = router;
